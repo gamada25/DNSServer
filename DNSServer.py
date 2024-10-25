3a6663401a750a974824229912bae38ae43b4c7f
gamada25 <gf2457@nyu.edu>
2024-10-25 17:20:40 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -1,6 +1,8 @@
 import dns.message
 import dns.rdatatype
 import dns.rdataclass
+import dns.rdtypes
+import dns.rdtypes.ANY
 from dns.rdtypes.ANY.MX import MX
 from dns.rdtypes.ANY.SOA import SOA
 import dns.rdata
@@ -9,12 +11,14 @@ import threading
 import signal
 import os
 import sys
+
 import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
 
+# Generate AES Key function
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -23,27 +27,33 @@ def generate_aes_key(password, salt):
         length=32
     )
     key = kdf.derive(password.encode('utf-8'))
-    return base64.urlsafe_b64encode(key)
+    key = base64.urlsafe_b64encode(key)
+    return key
 
+# Encrypt with AES
 def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    return f.encrypt(input_string.encode('utf-8')).decode('utf-8')
+    encrypted_data = f.encrypt(input_string.encode('utf-8'))
+    return encrypted_data    
 
+# Decrypt with AES
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    return f.decrypt(encrypted_data.encode('utf-8')).decode('utf-8')
+    decrypted_data = f.decrypt(encrypted_data)
+    return decrypted_data.decode('utf-8')
 
-# Prepare Encryption Parameters
-salt = b'Tandon'
-password = "gf2457@nyu.edu"
+# Parameters for encryption
+salt = b'Tandon'  # byte object
+password = "your_nyu_email@nyu.edu"  # replace with your NYU email
 input_string = "AlwaysWatching"
 
-# Encrypt the input string
+# Encrypt and decrypt the input string
 encrypted_value = encrypt_with_aes(input_string, password, salt)
+decrypted_value = decrypt_with_aes(encrypted_value, password, salt)
 
-# DNS records
+# DNS records setup
 dns_records = {
     'example.com.': {
         dns.rdatatype.A: '192.168.1.101',
@@ -62,18 +72,23 @@ dns_records = {
             86400,
         ),
     },
+    'safebank.com.': {dns.rdatatype.A: '192.168.1.102'},
+    'google.com.': {dns.rdatatype.A: '192.168.1.103'},
+    'legitsite.com.': {dns.rdatatype.A: '192.168.1.104'},
+    'yahoo.com.': {dns.rdatatype.A: '192.168.1.105'},
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.TXT: (encrypted_value.decode('utf-8'),),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-    }
+    },
 }
 
+# DNS Server Function
 def run_dns_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('0.0.0.0', 53))
+    server_socket.bind(("0.0.0.0", 53))
 
     while True:
         try:
@@ -103,17 +118,20 @@ def run_dns_server():
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
 
                 for rdata in rdata_list:
-                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
-                    response.answer[-1].add(rdata)
+                    rrset = dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype)
+                    rrset.add(rdata)
+                    response.answer.append(rrset)
 
-            response.flags |= dns.flags.AA
-            print("Responding to request:", qname)
+            response.flags |= 1 << 10  # Set AA (Authoritative Answer) flag
             server_socket.sendto(response.to_wire(), addr)
+            print("Responding to request:", qname)
+
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
             sys.exit(0)
 
+# Run server with user input to quit
 def run_dns_server_user():
     print("Input 'q' and hit 'enter' to quit")
     print("DNS server is running...")
@@ -132,3 +150,6 @@ def run_dns_server_user():
 
 if __name__ == '__main__':
     run_dns_server_user()
+    # Uncomment to see encrypted/decrypted values
+    # print("Encrypted Value:", encrypted_value)
+    # print("Decrypted Value:", decrypted_value)
